8ac507db74a6c173a8b18a0744d3b82d8b5099fe
Jesse Cotton <JCotton1123@gmail.com>
2015-03-15 14:55:03 -0400

    Load all Cake plugins by default

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -74,7 +74,7 @@ Cache::config('default', array('engine' => 'File'));
  *
  */
 
-CakePlugin::load('DebugKit');
+CakePlugin::loadAll();
 
 /**
  * You can attach event listeners to the request lifecyle as Dispatcher Filter . By Default CakePHP bundles two filters:
